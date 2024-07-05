59f19e70b6fb4f7a93054508de7b41d49b49c2fa
Sam Caldwell <github@samcaldwell.net>
2024-07-04 23:52:59 -0500

    fix missing test

--- Sha1_test.go
+++ Sha1_test.go
@@ -5,5 +5,8 @@ import (
 )
 
 func TestStateMetric_StateType_Sha1(t *testing.T) {
-
+	var s Sha1
+	if len(s) != Sha1Length {
+		t.Fatal("size mismatch")
+	}
 }
--- constants_test.go
+++ constants_test.go
@@ -6,6 +6,7 @@ import (
 
 func TestHashes_constants(t *testing.T) {
 	t.Run("Test Sha1Length", func(t *testing.T) {
+		// Expect 20 byte hash
 		const expectedSize = uint(20)
 		if expectedSize != Sha1Length {
 			t.Fatal("length is wrong")
